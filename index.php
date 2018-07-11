7040920e1f421d5092888c53a83094e278c1ead0
Roberto <rob.nettom@gmail.com>
2018-07-10 23:04:07 -0300

    really_finished_and_ok

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>ClientSys</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+  <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
     crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css?family=Roboto:300, 500" rel="stylesheet">
   <link rel="stylesheet" href="css/style.css" />
@@ -48,8 +48,8 @@ if (!$logged) {
     </div>
   </div>
   <?php include_once "include/footer.php"?>
-  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+  <script src="js/jquery-3.2.1.min.js"></script>
+  <script src="js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
     crossorigin="anonymous"></script>
   <?php
 if ($_GET) {
